fdd37a4ffa6e5e9a6c2d8dddd43e7046a45649a1
scintilla-nyctophilia <dessaalba08@gmail.com>
2018-10-06 17:21:14 +0800

    15th commit dessa

--- view/a_navigation.php
+++ view/a_navigation.php
@@ -12,6 +12,38 @@
     <!-- /.navbar-header -->
 
     <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-alerts">
+                <li>
+                    <a href="#">
+                        <div>
+                            <i class="fa fa-bell fa-fw"></i> New Comment
+                            <span class="pull-right text-muted small">4 minutes ago</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="#">
+                        <div>
+                            <i class="fa fa-bell fa-fw"></i> 3 New Followers
+                            <span class="pull-right text-muted small">12 minutes ago</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a class="text-center" href="index?route=notifications">
+                        <strong>See All Alerts</strong>
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </li>
+            </ul>
+            <!-- /.dropdown-alerts -->
+        </li>
         <!-- /.dropdown -->
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -69,7 +101,7 @@
                     <a href="index?route=tenants"><i class="fa fa-group fa-fw"></i> Tenants</a>
                 </li>
                 <li>
-                    <a href="index?route=payments"><i class="fa fa-ruble fa-fw"></i> Payments</a>
+                    <a href="index?route=payments"><i class="fa fa-ruble fa-fw"></i> Payments History</a>
                 </li>
                 <li>
                     <a href="index?route=complaints"><i class="fa fa-exclamation-triangle fa-fw"></i> Complaints</a>
